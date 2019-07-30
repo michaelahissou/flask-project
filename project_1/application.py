72b498631c15644585fb1d13dfc6323237f47fd6
michael <dossoumichae807@gmail.com>
2019-07-30 16:22:54 +0100

    fourth version

--- project_1/application.py
+++ project_1/application.py
@@ -11,13 +11,15 @@ def hello_world():
 def hello():
     return 'Hello, World'
 
-#@app.route("/michael")
-#def index():
-    #return "Hello, michael! "
+@app.route("/michael")
+def index():
+    return "Hello, michael , how are you ! "
 
-#@app.route("<string:name>")
-#def hello(name):
-#    return f" Hello,{name}!"
-@app.route('/user/<username>')
-def profile(username):
-    return '{}\'s profile'.format(escape(username))
+
+@app.route('/<string:name>')
+def name(name):
+    return f'hello, {name} '
+
+#@app.route('/user/<username>')
+#def profile(username):
+#   return '{}\'s profile'.format(escape(username))
