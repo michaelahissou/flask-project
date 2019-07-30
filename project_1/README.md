1a41f4d201a5678f39fa68559759050a8f08e1f1
michael <dossoumichae807@gmail.com>
2019-07-30 15:31:26 +0100

    third version

---
+++ project_1/README.md
@@ -0,0 +1,3 @@
+#Here is a simple web project written with flash.
+#Description
+In this project, we take in browser a user name and send him back a greeting word.
--- project_1/application.py
+++ project_1/application.py
@@ -2,14 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello, world! "
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-@app.route("/michael")
-def index():
-    return "Hello, michael! "
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+#@app.route("/michael")
+#def index():
+    #return "Hello, michael! "
 
 #@app.route("<string:name>")
 #def hello(name):
 #    return f" Hello,{name}!"
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
