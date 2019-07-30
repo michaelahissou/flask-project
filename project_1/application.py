79664c91c0923823ae013013ba3808c146419514
michael <dossoumichae807@gmail.com>
2019-07-30 13:59:08 +0100

    second stage

--- project_1/application.py
+++ project_1/application.py
@@ -1,11 +1,15 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route(''/'')
+@app.route("/")
 def index():
-    return ''Hello, world!''
+    return "Hello, world! "
 
-@app.route(''<string:name>'')
-def hello(name):
-    return f''Hello,{name}!''    
+@app.route("/michael")
+def index():
+    return "Hello, michael! "
+
+#@app.route("<string:name>")
+#def hello(name):
+#    return f" Hello,{name}!"
